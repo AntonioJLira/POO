688142bc2b56bab64a702d14e1c58d75edee4686
David Sánchez <i22safed@uco.es>
2016-09-22 16:32:22 +0200

    Modificado el fichero juego.cc

--- p1/juego.cpp
+++ p1/juego.cpp
@@ -4,14 +4,19 @@
 using namespace std;
 
 
-int main (){
+int main(void) {
 
-  Dados d;
+	Dados d;
+	
+	d.lanzamiento(); 
+	d.setDado1();
+	cout << "El valor del dado 1 es: " << d.getDado1() << "\n\n";
 
-  
+	d.setDado2(); 
+	cout << "El valor del dado 2 es: " << d.getDado2() << "\n\n"; 
+
+	cout << "La suma de los dados de los valores de los dados son: " << d.getSuma() << "\n\n" ; 
 
 
-
-
-  return 0;
+	return 0;
 }
