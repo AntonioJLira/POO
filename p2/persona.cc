7483b2e922d8fe2cab810708b9cc915d76fe8a9c
David <i22safed@uco.es>
2016-10-20 17:27:56 +0200

    Añadidos los constructores por defecto y parametros obligatorios

--- p2/persona.cc
+++ p2/persona.cc
@@ -3,6 +3,34 @@
 de una persona (todos datos de tipo string). */
 
 #include <iostream>
+#include <string>
 #include "persona.h"
 
-using namespace std; 
+
+using namespace std;
+
+
+Persona::Persona(string DNI, string nombre, string apellidos, string direccion,
+string localidad, string provincia, string pais){
+
+    DNI_=DNI;
+    nombre_ = nombre;
+    apellidos_ = apellidos;
+    direccion_ = direccion;
+    localidad_ = localidad;
+    provincia_ = provincia;
+    pais_ = pais;
+
+}
+
+Persona::Persona(){
+
+    DNI_="44XX";
+    nombre_ = "Carlos";
+    apellidos_ = "Gutierrez";
+    direccion_ = "C/ Mesa 1";
+    localidad_ = "Aguilar";
+    provincia_ = "Sevilla";
+    pais_ = "España";
+    
+}
--- p2/persona.h
+++ p2/persona.h
@@ -3,44 +3,46 @@
 
 #include <string>
 
-class Persona(){
+using namespace std;
+
+class Persona{
 
         public:
 
-            Persona(DNI); // Constructor
+            Persona(string DNI,string nombre="",string apellidos="",
+            string direccion="", string localidad="",string provincia="",
+            string pais=""); // Constructor con parametros por defecto
+
+            Persona(); // Constructor con parametros obligatorios
+
 
             // Modficadores → setters()
 
-            string setDNI();
-            string setNombre();
-            string setApellidos();
-            string setDireccion();
-            string setLocalidad();
-            string setProvincia();
-            string setPais();
+            void setDNI(string DNI){ DNI_ = DNI; };
+            void setNombre(string nombre){ nombre_ = nombre; };
+            void setApellidos(string apellidos){ apellidos_ = apellidos; };
+            void setDireccion(string direccion){ direccion_ = direccion; };
+            void setLocalidad(string localidad){ localidad_ = localidad; };
+            void setProvincia(string provincia){ provincia_ = provincia; };
+            void setPais(string pais){ pais_ = pais; };
 
             // Observadores → getters()
 
-            string getDNI();
-            string getNombre();
-            string getApellidos();
-            string getDireccion();
-            string getLocalidad();
-            string getProvincia();
-            string getPais();
+            string getDNI(){return DNI_;};
+            string getNombre(){return nombre_;};
+            string getApellidos(){return apellidos_; };
+            string getDireccion(){return direccion_; };
+            string getLocalidad(){return localidad_; };
+            string getProvincia(){return provincia_; };
+            string getPais(){return pais_; };
 
-            string getApellidosyNombre();
+            string getApellidosyNombre(){return getApellidos() + ", " + getNombre();};
 
         private:
 
             string DNI_ , nombre_ , apellidos_ , direccion_ , localidad_ ,
             provincia_ , pais_ ;
 
-
-
-
-
 };
-
 
 #endif
