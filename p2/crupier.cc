19514289e60fd77af18a1931d2c4d57923cb7657
David <i22safed@uco.es>
2016-10-24 18:06:47 +0200

    Finalizada la clase persona

---
+++ p2/crupier.cc
@@ -0,0 +1,10 @@
+
+#include "crupier.h"
+#include "persona.h"
+
+Crupier::Crupier(string DNI,string codigo,string nombre,string apellidos, string direccion, string localidad,string provincia,
+string pais):Persona(DNI,nombre,apellidos,direccion,localidad, provincia,pais){
+
+     codigo_=codigo;
+
+}
