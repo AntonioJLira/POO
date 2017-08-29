6d591ee95a63c23f76aa55033d1a5a2b42c9dbf5
David <i22safed@uco.es>
2017-08-29 20:08:21 +0200

    Añadido ejemplo sobre vectores de la STL

--- vectores/vector.cc
+++ vectores/vector.cc
@@ -5,9 +5,22 @@ using namespace std;
 
 int main(){
 
+     vector<int> v(10);
 
+     for(vector<int>::iterator i=v.begin();i<v.size();i++){
 
+          cout << "\nElemento 1: ";
+          cin >> v[i];
 
+     }
+
+     cout << "\n\nLos elementos del vector son: ";
+
+     for(vector<int>::iterator i=v.begin();i<v.size();i++){
+
+          cout << v[i] << " ";
+
+     }
 
 
 
